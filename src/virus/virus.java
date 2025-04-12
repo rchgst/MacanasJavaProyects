b54df1edd4eb240e90ceafed9ca6ba87ec0065f9
rchgst <quispericardo682@gmail.com>
2025-04-12 01:16:02 -0300

    estructure

---
+++ src/virus/virus.java
@@ -0,0 +1,17 @@
+package virus;
+
+public class virus {
+
+    private String nombre;
+    private int adn;
+    private boolean nocivo;
+
+    public virus(String nombre){
+        this.nombre = nombre;
+    }
+
+    public virus(){
+
+    }
+    
+}
