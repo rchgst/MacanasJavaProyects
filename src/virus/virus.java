eaa839741934b2c09b2a457d964625289c752968
rchgst <quispericardo682@gmail.com>
2025-04-12 02:12:55 -0300

    ejercicio realizado

--- src/virus/virus.java
+++ src/virus/virus.java
@@ -1,5 +1,7 @@
 package virus;
 
+
+
 public class virus {
 
     private String nombre;
@@ -7,11 +9,33 @@ public class virus {
     private boolean nocivo;
 
     public virus(String nombre){
+        this();
         this.nombre = nombre;
     }
 
     public virus(){
-
+        this.adn = (int)(Math.random()*10)+1;
+        this.nocivo = true;
     }
-    
+
+    public boolean esNocivo(){
+        return this.nocivo;
+    }
+
+    public void neutralizar(int adnNeutralizante){
+        if(adnNeutralizante == this.adn){
+            System.out.println("el virus fue neutralizado");
+            this.nocivo = true;
+        }
+        else{
+            System.out.println("el virus no fue neutralizado");
+            //System.err.println("el virus contenia el adn: "+ this.adn);
+        }
+    }
+
+    @Override
+    public String toString(){
+        return   "nombre del nombre ='" + this.nombre + ", adn=" + this.adn + ", nocivo=" + this.nocivo;
+    }
+
 }
