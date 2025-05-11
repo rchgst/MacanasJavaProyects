bf09a84de5ecea217aa1e9266dff93adeef948c1
Ivan Gomez <noob.programer.java@gmail.com>
2025-05-11 15:53:57 -0300

    "Add excercise of emulation of Virus"

---
+++ Virus/src/main/java/Virus.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class Virus {
+
+  public String nombre;
+  private int adn;
+  private boolean nocivo;
+
+  public Virus() {
+    Random random = new Random();
+    this.adn = random.nextInt(10) + 1;
+    this.nocivo = true;
+  }
+
+  public Virus(String nombre) {
+    this();
+    this.nombre = nombre;
+  }
+
+  public boolean esNocivo() {
+    return this.nocivo;
+  }
+
+  public void neutralizar(int adnNeutralizante) {
+    if (adnNeutralizante != this.adn) {
+      System.out.println("Lo siento, el virus continua siendo nocivo");
+    } else {
+      System.out.println("ADN encontrado! El virus ha sido neutralizado!");
+      this.nocivo = false;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "Virus [nombre=" + nombre + ", esNocivo()=" + esNocivo() + "]";
+  }
+
+}
